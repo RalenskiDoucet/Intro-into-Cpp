9f7e407e5c7aa50a7dac6eef2528d7292ff077ab
RalenskiDoucet <Ralenskid@gmail.com>
2017-08-14 17:46:51 -0500

    commit
    commit

---
+++ Homework 8-14-17/HomeWork 8-14-17.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void main()
+{
+	//You can not print to the console unless the problem specifies.
+
+	//1. Select suitable variable types and names for the following examples
+	//a) The average height of students in your class
+	float avgHeight;
+	//b) The tax rate for incomes over $65000
+	float taxrateIncomesOver65000;
+	//c) The tax rate for incomes below $5000
+	float rateBelow5k;
+	//d) The total cost of all the games on your Steam library
+	float costAllGames;
+
+	//2. Select suitable data types to hold the following values :
+	//a) 121
+	int;
+	//b) 9.4
+	float;
+	//c) r
+	char;
+	//d) 100000
+	unsigned Int;
+	//e) False
+	bool trueFalse = false
+
+		//3. Create a program that swaps two numbers. Ask the user for 2 numbers in the console window
+		//and store the result in variables called “a” and “b” print the result to screen.Write code 
+		//that swaps the values for these variables, then print the variables “a” and “b” again.
+		//You should comment your code.cin>> 18 cout << 9  cin >> 9 cout << 18
+
+	//4. Ask the user for 5 numbers and output the average of these numbers.		
+		10cout>>
+	//5. Redo question 3 but allows the user to input the values to be swapped and provide information
+	//in the console proving the data was swapped.
+}
+Add Comment Collapse
---
+++ Variables/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	//Class Example
+	//Interger data type 
+	int myNumber = 9;
+	//Floating point data type
+	float decimal = 0.5f;
+	//Character data type 
+	char myLetter = 'd';
+	//Boolean data type 
+	bool trueFalse = false;
+	system("pause");
+
+	//Consle read and write 
+	int Input;
+	std::cout << "Enter to number / n";
+	std::cin >> Input >> Input;
+
+	std::cout << Input << "" << Input;
