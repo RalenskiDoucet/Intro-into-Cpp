2eb07300b6ac6bb65b85e695d8ef65f761de71a9
RalenskiDoucet <Ralenskid@gmail.com>
2017-08-21 11:37:42 -0500

    I commit

--- Helloworld/Conditionals Classwork.cpp
+++ Helloworld/Conditionals Classwork.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 int main()
+
 {
 	//1. What do the following conditionals evaluate to if "A = true and B = false"
 	// A || B
@@ -23,55 +23,73 @@ int main()
 
 	//2. Write an if statement that assigns 100 to the value of x when y is equal to zero
 	bool condition = true;
-	if ("|int y=0 and x=100") {}
+	if ("|int y=0 and x=100"){}
 
-		//3. Write a program that accepts an integer that represents the month of the year. 
+
+	//3. Write a program that accepts an integer that represents the month of the year. 
 		//It should then display the number of days in that month. If a number that doesn’t correspond 
 		//to a month is entered then the program should display an error message. You must use a switch statement
-		//  takes user input number .
 
-	int month;
-	std::cin >> month;
+
+	{// takes user input number the input number must be 1-12.
+
+		int sumNumber;
+		std::cin >> sumNumber;
+
+
 		// when the user enters 1 the month January is the output answer.
-	    // user must enter an number 1-12.
-	int sumnumber; 
-	std::cin >> sumnumber;
-	switch (sumnumber) {
-	case 1:
-		std::cout <<2;
+		// user must enter an number 1-12.
+		int sumNumber;
+		std::cin>> sumNumber;
+		switch (sumNumber);
+		
+		case 1: 
+		//When the user enters the number 2 Febraury is the output answer.
+		std::cout <<28; 
 		break;
-	case 2:
-		std::cout << 3;
+		case 2:
+		//When the user enters the number 3 March is the output answer.
+		std::cout << 31;
 		break;
-	case 3:
-		std::cout << 4;
+		case 3:
+		//When the user enters the number 4 April is the output answer.
+		std::cout <<30;
 		break;
-	case 4 :
-	std::cout << 5;
-	break;
-	case 5:
-		std::cout << 6;
+		case 4 :
+		//When the user enters the number 5 May is the output answer.
+		std::cout <<31;
 		break;
-	case 6:
-		std::cout << 7;
+		case 5:
+		//When the user enters the number 6 June is the output answer.
+		std::cout << 30;
 		break;
-	case 7:
-		std::cout << 8;
+		case 6:
+		//When the user enters the number 7 July is the output answer.
+		std::cout <<31;
 		break;
-	case 8:
-		std::cout << 9;
+		case 7:
+		//When the user enters the number 8 August is the output answer.
+		std::cout << 31;
 		break;
-	case 9:
-		std::cout << 10;
+		case 8:
+		//When the user enters the number 9 September is the output answer.
+		std::cout <<31;
 		break;
-	case 10:
-		std::cout << 11;
+		case 9:
+		//When the user enters the number 10 October is the output answer.
+		std::cout <<30;
 		break;
-	case 11:
-		std::cout << 12;
+		case 10:
+		//When the user enters the number 11 November is the output answer.
+		std::cout << 31;
 		break;
-	case 12:
-		std::cout << 13;
+		case 11:
+		//When the user enters the number 12 Decmber is the output answer.
+		std::cout << 30;
+		break;
+		case 12:
+		//When the user enters the number	 is the output answer.
+		std::cout << 31;
 	}
 
 
@@ -81,4 +99,3 @@ int main()
 
 		//5. Convert problem 2 into a ternary operator
 
-}
