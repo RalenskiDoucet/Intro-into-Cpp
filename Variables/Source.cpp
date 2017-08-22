4254345de8bafe9c913d65aafaa1ed30362091ae
RalenskiDoucet <Ralenskid@gmail.com>
2017-08-22 17:52:47 -0500

    i commit
    varibles Commit

--- Variables/Source.cpp
+++ Variables/Source.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
 
-int main()
+void main()
 {
-	//Class Example
-	//Interger data type 
-	int myNumber = 9;
-	//Floating point data type
-	float decimal = 0.5f;
-	//Character data type 
-	char myLetter = 'd';
-	//Boolean data type 
-	bool trueFalse = false;
-	system("pause");
+	//You can not print to the console unless the problem specifies.
 
-	//Consle read and write 
-	int Input;
-	std::cout << "Enter to number / n";
-	std::cin >> Input >> Input;
+	//1. Select suitable variable types and names for the following examples
+	//a) The average height of students in your class
+	int avgHight = 6
+		//b) The tax rate for incomes over $65000
+		int taxRate = 65000
+		//c) The tax rate for incomes below $5000
+		int rateOftax = 5000
+		//d) The total cost of all the games on your Steam library
+		int totalCall =
 
-	std::cout << Input << "" << Input;
+		//2. Select suitable data types to hold the following values :
+		//a) 121
+		int
+		//b) 9.4
+		float
+		//c) r
+		char
+		//d) 100000
+		int
+		//e) False
+		bool
+
+		//3. Create a program that swaps two numbers. Ask the user for 2 numbers in the console window
+		//and store the result in variables called “a” and “b” print the result to screen.Write code 
+		//that swaps the values for these variables, then print the variables “a” and “b” again.
+		//You should comment your code.
+		int sumNum = a;
+		int sumNumber = b;
+		int openBox = c;
+		std::cin >> sumNum;
+		std::cin>> sumNumber;
+		int a = c;
+		int c = a;
+		int a = b;
+	//4. Ask the user for 5 numbers and output the average of these numbers.		
+		std::cin >> Num;
+		std::cin >>nUm;
+
+	//5. Redo question 3 but allows the user to input the values to be swapped and provide information
+	//in the console proving the data was swapped.
+	
