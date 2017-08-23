05ffa7f9b9ea8eebe18184cc3897afa730c417cc
RalenskiDoucet <Ralenskid@gmail.com>
2017-08-23 10:09:25 -0500

    intial Commit

--- Helloworld/Conditionals Classwork.cpp
+++ Helloworld/Conditionals Classwork.cpp
@@ -35,72 +35,75 @@
 		// Ask the user to input a number the input number.
 		// The number the user inputs must be between 1-12.
 
-		"Please Input a number." std::cin >> sumNumber;
-	}
-		int sumNumber;
+		//Please Input a number.	
+		int monthName;
+		int sumNumber;		
+		std::cin >> monthName;
 		std::cin >> sumNumber;
+		switch (monthName);
+			   (sumNumber);
 
 
-		// when the user enters 1 the month January is the output answer.
+		// when the user enters 1 the month January is output number 1 answer.
 		// user must enter an number 1-12.
-		int sumNumber;
-		std::cin>> sumNumber;
-		switch (sumNumber)
+			 std
+
+		 
 
 		case 1:
 		//When the user enters the number 2 Febraury is the output answer;
-		std::cout < 28; 
-		break
+		std::cout <<28; 
+		break;
 		case 2:
 		//When the user enters the number 3 March is the output answer.
 	
-			std::cout << "31"; 
+			std::cout << 31; 
 		break;
 		case 3:
 		//When the user enters the number 4 April is the output answer.
 			
-		std::cout <<"30";
+		std::cout <<30;
 		break;
 		case 4 :
 		//When the user enters the number 5 May is the output answer.
 		;
-		std::cout <<"31";
+		std::cout <<31;
 		break;
 		case 5:
 		//When the user enters the number 6 June is the output answer.
 		
-		std::cout << "30";
+		std::cout << 30;
 		break;
 		case 6:
 		//When the user enters the number 7 July is the output answer.
 		std::
 		
-		std::cout <<"31";
+		std::cout <<31;
 		break;
 		case 7:
 		//When the user enters the number 8 August is the output answer.
 			
-		std::cout << "31";
+		std::cout << 31;
 		break;
 		case 8:
 		//When the user enters the number 9 September is the output answer.
 		
-		std::cout <<"31";
+		std::cout <<31;
 		break;
 		case 9:
 		//When the user enters the number 10 October is the output answer.
-		std::cout <<"30";
+		std::cout <<30;
 		break;
 		case 10:
 		//When the user enters the number 11 November is the output answer.
 			
-		std::cout << "31";
+		std::cout << 31;
 		break;
 		case 11:
 		//When the user enters the number 12 Decmber is the output answer.
-			
-		std::cout << "30";
-		break;
+		
+			std::cout << 30;
+			break; 
 		
 	}
 
@@ -126,7 +129,7 @@
 		if:
 		//When the user enters the number 2 Febraury is the output answer.
 		std::cout << "Febraury";
-		std::cout << "28";
+		std::cout <<sumNumber "28";
 		break;
 		if:
 		//When the user enters the number 3 March is the output answer.
